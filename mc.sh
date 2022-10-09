abd6dba2db5ba141a0ea4b8c1e8ce568a2ed76c4
Nathan Gelman <ngelman@valorem.com>
2022-10-09 12:28:05 -0700

    working minecraft server

--- mc.sh
+++ mc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+sudo apt update
+sudo apt upgrade -y
 
+MINECRAFT_FILE=bedrock-server-1.19.31.01.zip
 sudo ufw allow 19132/udp
-curl -O https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.31.01.zip
-sudo apt-get install unzip
-sudo unzip bedrock-server-1.19.31.01.zip -d server
+curl -O https://minecraft.azureedge.net/bin-linux/$MINECRAFT_FILE
+sudo apt install unzip
+unzip $MINECRAFT_FILE -d server
+sudo rm $MINECRAFT_FILE
 
 echo "[Unit]" >> temp
 echo "[Service]" >> temp
@@ -12,7 +16,7 @@ echo "User=ngelman" >> temp
 echo "ExecStart=/home/ngelman/start.sh" >> temp
 echo "[Install]" >> temp
 echo "WantedBy=multi-user.target" >> temp
-mv temp /lib/systemd/system/minecraft.service
+sudo mv temp /lib/systemd/system/minecraft.service
 
 echo "#!/bin/bash" >> temp
 echo "SESSIONNAME=\"minecraft\"" >> temp
@@ -22,6 +26,11 @@ echo "  then" >> temp
 echo "    tmux new-session -s \$SESSIONNAME -n script -d" >> temp
 echo "    tmux send-keys -t \$SESSIONNAME \"cd /home/ngelman/server && ./start.sh\" C-m" >> temp
 echo "fi" >> temp
+sudo mv temp ~/start.sh
+sudo chmod +x ~/start.sh
+
+echo "#!/bin/bash" >> temp
+echo "LD_LIBRARY_PATH=. ./bedrock_server" >> temp
 mv temp ~/server/start.sh
 sudo chmod +x ~/server/start.sh
 
